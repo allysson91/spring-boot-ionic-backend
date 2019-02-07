60e7ccd0f592f46374169416dfb9454d6e8ccc9e
Allysson Oliveira <allysson.jouglas@gmail.com>
2019-02-07 18:57:41 -0200

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/allysson/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.allysson.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.allysson.cursomc.domain.Cliente;
+import com.allysson.cursomc.repositories.ClienteRepository;
+import com.allysson.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
