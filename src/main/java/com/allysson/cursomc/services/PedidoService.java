15e714d2c357e4343a232cb46b2d2e10f78fd200
Allysson Oliveira <allysson.jouglas@gmail.com>
2019-02-08 12:52:30 -0200

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/allysson/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.allysson.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.allysson.cursomc.domain.Pedido;
+import com.allysson.cursomc.repositories.PedidoRepository;
+import com.allysson.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+}
