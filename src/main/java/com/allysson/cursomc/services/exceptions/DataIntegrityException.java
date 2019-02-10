c1104b0d51b332ab4c8b22337dbf3d48f0599458
Allysson Oliveira <allysson.jouglas@gmail.com>
2019-02-09 23:05:58 -0200

    Deletando uma Categoria com DELETE

---
+++ src/main/java/com/allysson/cursomc/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,18 @@
+package com.allysson.cursomc.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DataIntegrityException(String msg) {
+		super(msg);
+	}
+	
+	public DataIntegrityException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
